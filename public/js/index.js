af958634f33e733e7abad6ff53e3cea8e8aea8a0
Mario Roque <A01282301@itesm.mx>
2020-05-09 22:59:52 -0500

    make it better

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,1 @@
 const API_TOKEN = "2abbf7c3-245b-404f-9473-ade729ed4653";
-
-function fetchAllBookmarks(){
-    let url = '/bookmarks';
-    let settings = {
-        method: 'GET',
-        headers: {
-            Authorization: `Bearer ${API_TOKEN}`
-        }
-    }
-    
